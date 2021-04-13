70a934698db15f4429c4bdd7cd9fec6a31aaf0f6
Min Jean Cho <minjeancho@Mins-MacBook-Pro.local>
2021-04-12 22:30:43 -0400

    baseline added

---
+++ deepfake_autoencoder/preprocess.py
@@ -0,0 +1,13 @@
+from torch.utils.data import Dataset
+
+#TODO preprocess data 
+class DeepfakeDataset(Dataset):
+    def __init__(self):
+        #output image should be of size 64x64x3 (height x width x num_channels)
+        pass
+    def __len__(self):
+        pass
+    
+    def __getitem__(self, idx):
+        pass
+    
