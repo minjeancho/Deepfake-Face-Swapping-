bc099b478f938c3acd5fa11074f756054d838c99
lushao1232 <lushao.ls1232@gmail.com>
2021-04-13 12:22:51 -0400

    D linear

--- deepfake_autoencoder/discriminator.py
+++ deepfake_autoencoder/discriminator.py
@@ -6,21 +6,26 @@ class Discriminator(nn.Module):
     super().__init__()
 
     self.model = nn.Sequential(
-        nn.Conv2d(3, 6, 4),
+        nn.Conv2d(3, 6, 4, 2, 1, bias = False),
         nn.BatchNorm2d(6),
         nn.LeakyReLU(0.2),
 
-        nn.Conv2d(6, 64, 4),
+        nn.Conv2d(6, 64, 4, 2, 1, bias = False),
         nn.BatchNorm2d(64),
         nn.LeakyReLU(0.2),
 
-        nn.Conv2d(64, 128, 4),
+        nn.Conv2d(64, 128, 4, 2, 1, bias = False),
         nn.BatchNorm2d(128),
         nn.LeakyReLU(0.2),
 
-        nn.Conv2d(128, 256, 5),
+        nn.Conv2d(128, 256, 5, 2, 1, bias = False),
         nn.BatchNorm2d(256),
-        nn.LeakyReLU(0.2)
+        nn.LeakyReLU(0.2),
+
+        nn.Flatten(),
+        nn.Sigmoid(),
+        ## need to calculate number of in_features
+        nn.Linear( , 1)
     )
 
   def forward(self, x):
